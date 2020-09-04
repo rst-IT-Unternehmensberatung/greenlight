3d650abca99eb12e298f175d36d5dd4bc1b4b388
MrKeksi <x.treme.field@gmx.de>
2020-09-04 08:15:44 +0200

    recording?

--- app/controllers/concerns/joiner.rb
+++ app/controllers/concerns/joiner.rb
@@ -89,7 +89,7 @@ module Joiner
     {
       user_is_moderator: false,
       meeting_logout_url: request.base_url + logout_room_path(@room),
-      meeting_recorded: true,
+      meeting_recorded: @room.recording?,
       moderator_message: "#{invite_msg}\n\n#{request.base_url + room_path(@room)}",
       host: request.host,
       recording_default_visibility: @settings.get_value("Default Recording Visibility") == "public"
